a6a791d9d3480cebe99039cced07326ef8aae302
Szymon Studniarz <szymek1019@gmail.com>
2025-02-01 16:28:09 +0100

    add funcs for employee create, findByLogin and getAll

--- backend/db/createUser.js
+++ backend/db/createUser.js
@@ -1,20 +1,33 @@
 const db = require('./database');
 
-async function createUser(name, email) {
+async function createEmployee(name, surname, login, password, pesel) {
     try {
-        const result = await db.one('INSERT INTO users(name, surname, pesel, login, password) VALUES($1, $2) RETURNING *', [name, email]);
+        // const result = await db.one('INSERT INTO users(name, surname, pesel, login, password) VALUES($1, $2) RETURNING *', [name, email]);
+        const result = await db.one('INSERT INTO employee (name,surname,login,password,pesel) VALUES($1, $2,$3,$4,$5) ON CONFLICT(login) DO NOTHING',[name, surname, login, password, pesel])
         console.log(result);
     } catch (error) {
         console.error('Error creating user:', error);
     }
 }
 
-db.one('INSERT INTO employee (name,surname,pesel,login,password) VALUES($1, $2,$3,$4,$5) ON CONFLICT(login,pesel) DO NOTHING',["Jan","Kowalik","123123124","janeq","test"])
-.then((data)=>{
-    console.log('DATA:',data)
-})
-.catch((error) => {
-    console.log('ERROR:', error)
-  })
+async function getAllEmployees() {
+    try {
+        const result = await db.any('SELECT * FROM employee');
+        console.log(result);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
+}
 
-module.exports = { createUser };
+async function findEmployeeByLogin(login) {
+    try {
+        const result = await db.oneOrNone(`SELECT * FROM employee WHERE login = $1`, [login]);
+        // console.log(result);
+        return result
+    } catch (error) {
+        console.error('Error finding user:', error);
+    }
+}
+
+
+module.exports = { createEmployee, getAllEmployees, findEmployeeByLogin };
